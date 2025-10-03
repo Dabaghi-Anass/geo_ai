25c31ed2fa984a27571e9c6c73fda06f6ad8faf9
anassdabaghi <anass.debbaghi123@gmail.com>
2025-10-04 00:10:46 +0100

    changes

--- server/download_model.py
+++ server/download_model.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import rarfile
 
 MODEL_URL    = "https://drive.google.com/uc?export=view&id=1oZB-6mNWy4Qwj7DjwAh4v4uctFme7LOO"
 MODEL_PATH   = "best_model.rar"
@@ -18,15 +17,6 @@ def download_model_if_needed():
     else:
         print("Model already exists.")
 
-def extract_model():
-    if not os.path.isdir(EXTRACT_DIR):
-        print(f"Extracting {MODEL_PATH} → {EXTRACT_DIR}/")
-        with rarfile.RarFile(MODEL_PATH) as rf:
-            rf.extractall(EXTRACT_DIR)
-        print("Extraction complete.")
-    else:
-        print("Archive already extracted.")
 
 if __name__ == "__main__":
     download_model_if_needed()
-    extract_model()
